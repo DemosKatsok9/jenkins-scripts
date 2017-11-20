0b4c129da156d07f4063080e115cfd52d9b72792
katsok9 <katsok9@gmail.com>
2017-11-20 13:12:41 +0200

    add stages

--- vars/yalla.groovy
+++ vars/yalla.groovy
@@ -3,9 +3,13 @@ import com.katsok9.demo.YallaRunner
 def call(projectDefs) {
     timestamps {
         node {
-            checkout scm
+            stage('checkout') {
+                checkout scm
+            }
            // YallaRunner.yalla(this, projectDefs)
-            printColor("yellow", "this is the dev")
+            stage('Who Am I') {
+                printColor("yellow", "I am dev")
+            }
         }
     }
 }
