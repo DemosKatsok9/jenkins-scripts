bbf43af5f5562053cfaaf1e362fb2625e559dbf8
katsok9 <katsok9@gmail.com>
2017-11-20 13:10:03 +0200

    add stages

--- vars/yalla.groovy
+++ vars/yalla.groovy
@@ -1,14 +1,18 @@
 import com.katsok9.demo.MavenArtifactoryUtil
-import com.katsok9.demo.YallaRunner
 
 def call(projectDefs) {
     timestamps {
         node {
-            checkout scm
+            stage('checkout') {
+                checkout scm
+                printColor("blue", "I'm the 0.2.1-SNAPSHOT")
+            }
 //            YallaRunner.yalla(this, projectDefs)
-            printColor("blue","I'm the 0.2.1-SNAPSHOT")
-            MavenArtifactoryUtil.buildArtifactory(this,false)
+            stage('build') {
+                MavenArtifactoryUtil.buildArtifactory(this, false)
+            }
         }
     }
 }
+
 return this
