e97e63425a1640cedd188b76637ce1bac437c10d
katsok9 <katsok9@gmail.com>
2017-11-20 15:45:19 +0200

    comment yall

--- src/com/katsok9/demo/MapperUtil.groovy
+++ src/com/katsok9/demo/MapperUtil.groovy
@@ -4,15 +4,19 @@ class MapperUtil implements Serializable {
 
     static def mapdefs(_pipe, projectDefs) {
         def steps = [:]
-//        switch (projectDefs.language) {
-//            case "java": steps.java = new MavenArtifactoryUtil(_pipe, projectDefs); break
-//            case "node_js": steps.node_js = new NpmNodeJSUtil(_pipe, projectDefs); break
-//            case "my_step": steps.my_stem = new MyStep(_pipe, projectDefs); break
-//            default: _pipe.printColor("orange", "Not yet implemented")
-//        }
-        _pipe.printColor("blue", "each: ${projectDefs}")
-        projectDefs.each{pdef ->
-            _pipe.printColor("blue","${pdef.key}")
+        switch (projectDefs.language) {
+            case "java": steps.java = new MavenArtifactoryUtil(_pipe, projectDefs); break
+            case "node_js": steps.node_js = new NpmNodeJSUtil(_pipe, projectDefs); break
+            default: _pipe.printColor("orange", "Not yet implemented")
+        }
+        projectDefs.each { key, value ->
+            _pipe.printColor("blue", "key: ${key}, val: ${value}")
+            switch (key) {
+                case "version": _pipe.printColor("orange","warn: will support @grab here"); break
+                case "my_step": steps.my_stem = new MyStep(_pipe, projectDefs); break
+                default: _pipe.printColor("orange", "Not yet implemented")
+            }
+
         }
         return steps
     }
