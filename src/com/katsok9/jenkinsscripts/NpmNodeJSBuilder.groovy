7fcca25c45b6980b658c1be34a160d79c03e85ec
katsok9 <katsok9@gmail.com>
2017-11-20 16:40:57 +0200

    remove noise

--- src/com/katsok9/jenkinsscripts/NpmNodeJSBuilder.groovy
+++
@@ -1,9 +0,0 @@
-package com.katsok9.jenkinsscripts
-
-class NpmNodeJSBuilder implements Phase {
-
-    NpmNodeJSBuilder(__pipe__,  p) {
-
-        PipeGlobal.printColor(__pipe__,"not yet implemented", "orange")
-    }
-}
