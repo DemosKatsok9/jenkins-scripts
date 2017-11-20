de8b8b09b6adbc078cb1d13e68abbd5f1ddcc4f8
katsok9 <katsok9@gmail.com>
2017-11-20 16:18:20 +0200

    check jenkins file

--- src/com/katsok9/demo/StepsRunner.groovy
+++ src/com/katsok9/demo/StepsRunner.groovy
@@ -1,10 +1,9 @@
 package com.katsok9.demo
 
 
-
-    static def yalla(Object projectDefs) {
-        printColor("blue", "${projectDefs}")
-        printColor("blue", "In runner")
+static def yalla(Object projectDefs) {
+    printColor("blue", "${projectDefs}")
+    printColor("blue", "In runner")
 //        JobStep steps = MapperUtil.mapdefs(_pipe, projectDefs)
 //        steps.each { step ->
 //            _pipe.stage("${step.name}") {
@@ -12,5 +11,5 @@ package com.katsok9.demo
 //            }
 //        }
 
-    }
 }
+
