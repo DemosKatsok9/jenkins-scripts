263715c7158029fd9ce4b32592be5198a6bc1547
katsok9 <katsok9@gmail.com>
2017-11-20 15:57:36 +0200

    add each

--- src/com/katsok9/demo/MapperUtil.groovy
+++ src/com/katsok9/demo/MapperUtil.groovy
@@ -4,20 +4,20 @@ class MapperUtil implements Serializable {
 
     static def mapdefs(_pipe, projectDefs) {
         List<JobStep> steps = Collections.emptyList()
-//        switch (projectDefs.language) {
-//            case "java": steps.java = new MavenArtifactoryUtil(_pipe, projectDefs); break
-//            case "node_js": steps.node_js = new NpmNodeJSUtil(_pipe, projectDefs); break
-//            default: _pipe.printColor("orange", "Not yet implemented")
-//        }
-//        projectDefs.keySet() { key ->
-//            _pipe.printColor("blue", "key: ${key}, val: ${projectDefs.get(key)}")
-//            switch (key) {
-//                case "version": _pipe.printColor("orange","warn: will support @grab here"); break
-//                case "my_step": steps.put('my_step', new MyStep()); break
-//                default: _pipe.printColor("orange", "Not yet implemented")
-//            }
-//
-//        }
+        switch (projectDefs.language) {
+            case "java": steps.java = new MavenArtifactoryUtil(_pipe, projectDefs); break
+            case "node_js": steps.node_js = new NpmNodeJSUtil(_pipe, projectDefs); break
+            default: _pipe.printColor("orange", "Not yet implemented")
+        }
+        projectDefs.keySet().each { key ->
+            _pipe.printColor("blue", "key: ${key}, val: ${projectDefs.get(key)}")
+            switch (key) {
+                case "version": _pipe.printColor("orange","warn: will support @grab here"); break
+                case "my_step": steps.put('my_step', new MyStep()); break
+                default: _pipe.printColor("orange", "Not yet implemented")
+            }
+
+        }
         return steps
     }
 
