ba5352cdb3931a4786838b76b355e30378d11129
katsok9 <katsok9@gmail.com>
2017-11-20 15:54:13 +0200

    comment yall

--- src/com/katsok9/demo/MapperUtil.groovy
+++ src/com/katsok9/demo/MapperUtil.groovy
@@ -9,15 +9,15 @@ class MapperUtil implements Serializable {
 //            case "node_js": steps.node_js = new NpmNodeJSUtil(_pipe, projectDefs); break
 //            default: _pipe.printColor("orange", "Not yet implemented")
 //        }
-        projectDefs.keySet() { key ->
-            _pipe.printColor("blue", "key: ${key}, val: ${projectDefs.get(key)}")
-            switch (key) {
-                case "version": _pipe.printColor("orange","warn: will support @grab here"); break
-                case "my_step": steps.put('my_step', new MyStep()); break
-                default: _pipe.printColor("orange", "Not yet implemented")
-            }
-
-        }
+//        projectDefs.keySet() { key ->
+//            _pipe.printColor("blue", "key: ${key}, val: ${projectDefs.get(key)}")
+//            switch (key) {
+//                case "version": _pipe.printColor("orange","warn: will support @grab here"); break
+//                case "my_step": steps.put('my_step', new MyStep()); break
+//                default: _pipe.printColor("orange", "Not yet implemented")
+//            }
+//
+//        }
         return steps
     }
 
