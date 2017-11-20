fe1eacb3b7625bc8b4906221589188f30b89e4b0
katsok9 <katsok9@gmail.com>
2017-11-20 13:08:07 +0200

    fix this

--- vars/yalla.groovy
+++ vars/yalla.groovy
@@ -7,7 +7,7 @@ def call(projectDefs) {
             checkout scm
 //            YallaRunner.yalla(this, projectDefs)
             printColor("blue","I'm the 0.2.1-SNAPSHOT")
-            MavenArtifactoryUtil.buildArtifactory(_pipe,false)
+            MavenArtifactoryUtil.buildArtifactory(this,false)
         }
     }
 }
